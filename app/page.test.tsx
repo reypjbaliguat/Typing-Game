67e9e7510076fd41ad324930a8a6ef9928120d58
reypjbaliguat <124473819+reypjbaliguat@users.noreply.github.com>
2023-04-01 21:20:18 +0800

    prettier added

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -2,15 +2,16 @@ import "@testing-library/jest-dom";
 import { render, screen } from "./__test__/test-utils";
 import Home from "./page";
 import { act } from "react-dom/test-utils";
-import { useDispatch } from "react-redux";
 import { fetchText } from "../features/text";
-import { AppDispatch } from "./store/store";
 
 describe("Home Page", () => {
-    test("Renders Home page", () => {
-        render(<Home />);
-        const dispatch = useDispatch<AppDispatch>();
-        act(() => dispatch(fetchText()));
+    test("Renders Home page", async () => {
+        // const dispatch = useDispatch<AppDispatch>();
+        // const [componentTextValue, setComponentTextValue] = useState("");
+        await act(() => {
+            render(<Home />);
+            fetchText();
+        });
 
         const main = screen.getByRole("main");
         expect(main).toBeInTheDocument();
